890bcb1b9b371d6dc94bcb9eb64f3c03924d3fb8
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini.local>
2015-10-01 15:30:43 -0400

    use ticker TETH

--- app/js/ticker.js
+++ app/js/ticker.js
@@ -3,8 +3,8 @@ angular.module('safemarket').service('ticker',function($interval,$http,$q){
 	web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 	var tickerAddress = "0xdc99b79555385ab2fe0ff28c3c954a07b28aac5e"
-		,symbols = ['CMC:ETH:USD','CMC:ETH:EUR','CMC:ETH:CNY','CMC:ETH:CAD','CMC:ETH:RUB','CMC:ETH:BTC']
-		,rates = {'ETH':new BigNumber(1)}
+		,symbols = ['CMC:TETH:USD','CMC:TETH:EUR','CMC:TETH:CNY','CMC:TETH:CAD','CMC:TETH:RUB','CMC:TETH:BTC']
+		,rates = {'ETH':new BigNumber('1')}
 		,OpenStoreAbi = [{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"key","type":"bytes32"}],"name":"getTimestamp","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes32"},{"name":"value","type":"bytes"}],"name":"setFromContract","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"key","type":"bytes32"}],"name":"getValue","outputs":[{"name":"","type":"bytes"}],"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes32"},{"name":"value","type":"bytes"}],"name":"set","outputs":[],"type":"function"}]
    		,OpenStore = web3.eth.contract(OpenStoreAbi).at("0xaf527686227cc508ead0d69c7f8a98f76b63e191")
 
