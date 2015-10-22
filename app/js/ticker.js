d19684417e32910a41927369fa84e35bb05bce4c
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini.local>
2015-10-22 13:42:58 -0400

    default to OpenStore with default account as namespace for now

--- app/js/ticker.js
+++ app/js/ticker.js
@@ -1,6 +1,6 @@
 (function(){
 angular.module('safemarket').service('ticker',function($interval,$http,$q){
-  var tickerAddress = ((OpenStoreNamespace.address != '0' && OpenStoreNamespace.address) || web3.eth.accounts[0]);
+  var tickerAddress = web3.eth.accounts[0] || ((OpenStoreNamespace.address != '0' && OpenStoreNamespace.address) || web3.eth.accounts[0]);
 	var symbols = ['CMC:TETH:USD','CMC:TETH:EUR','CMC:TETH:CNY','CMC:TETH:CAD','CMC:TETH:RUB','CMC:TETH:BTC'];
 	var rates = {'ETH': (new BigNumber('1'))};
 
