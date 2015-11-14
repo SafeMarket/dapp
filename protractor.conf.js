e00c73bef4323e871718578a88ce303dc122322f
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini.local>
2015-11-13 20:40:20 -0500

    enable all specs

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,9 +2,9 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub'
 	,specs: [
 		'spec/auth.spec.js'
-		// ,'spec/settings.spec.js'
-		// ,'spec/market.spec.js'
-		// ,'spec/store.spec.js'
+		,'spec/settings.spec.js'
+		,'spec/market.spec.js'
+		,'spec/store.spec.js'
 	]
 	,allScriptsTimeout: 20000
 	,onPrepare: function() {
