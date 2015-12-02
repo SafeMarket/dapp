60f46d8779c1755365d6743013e7bde287ec8882
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini-2.local>
2015-12-02 13:31:22 -0500

    use correct OpenStore contract in production

--- app/js/services/ticker.js
+++ app/js/services/ticker.js
@@ -2,9 +2,14 @@
 
 angular.module('safemarket').service('ticker',function($interval,$http,$q){
 
-	//TODO: OpenStore namespace will be wrong in production
+	if(window.module){ //if production
+		var OpenStoreAbi = [{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"key","type":"bytes32"}],"name":"getTimestamp","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes32"},{"name":"value","type":"bytes"}],"name":"setFromContract","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"key","type":"bytes32"}],"name":"getValue","outputs":[{"name":"","type":"bytes"}],"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes32"},{"name":"value","type":"bytes"}],"name":"set","outputs":[],"type":"function"}]
+    		,OpenStore = web3.eth.contract(OpenStoreAbi).at("0xaf527686227cc508ead0d69c7f8a98f76b63e191")
+			,tickerAddress = '0xdc99b79555385ab2fe0ff28c3c954a07b28aac5e'
+	}else{
+		var tickerAddress = web3.eth.accounts[0] || ((OpenStoreNamespace.address != '0' && OpenStoreNamespace.address) || web3.eth.accounts[0]);
+	}
 
-  	var tickerAddress = web3.eth.accounts[0] || ((OpenStoreNamespace.address != '0' && OpenStoreNamespace.address) || web3.eth.accounts[0]);
 	var symbols = ['CMC:TETH:USD','CMC:TETH:EUR','CMC:TETH:CNY','CMC:TETH:CAD','CMC:TETH:RUB','CMC:TETH:BTC'];
 	var rates = {'ETH': (new BigNumber('1'))};
 
