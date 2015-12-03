1b055a041f55f5ac59b52fa8c89c272cbb5d7667
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini-2.local>
2015-12-03 15:44:39 -0500

    drop unecessary icons

--- app/js/services/ticker.js
+++ app/js/services/ticker.js
@@ -2,7 +2,7 @@
 
 angular.module('safemarket').service('ticker',function($interval,$http,$q){
 
-	if(window.module){ //if production
+	if(blockchain.env==='production'){
 		var OpenStoreAbi = [{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"key","type":"bytes32"}],"name":"getTimestamp","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes32"},{"name":"value","type":"bytes"}],"name":"setFromContract","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"key","type":"bytes32"}],"name":"getValue","outputs":[{"name":"","type":"bytes"}],"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes32"},{"name":"value","type":"bytes"}],"name":"set","outputs":[],"type":"function"}]
     		,OpenStore = web3.eth.contract(OpenStoreAbi).at("0xaf527686227cc508ead0d69c7f8a98f76b63e191")
 			,tickerAddress = '0xdc99b79555385ab2fe0ff28c3c954a07b28aac5e'
