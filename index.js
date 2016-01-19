45d2ec6cabf0f72101927ab243907fea90102518
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini-2.local>
2016-01-18 19:28:43 -0500

    cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-var Geth = require('./modules/geth.js')
-
-geth = new Geth('geth','--datadir /Users/aakilfernandes/SafeMarket/')
-
-geth.quickstart('password').then(function(){
-	console.log('yeah!!!')
-})
