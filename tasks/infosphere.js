b3c785024ae590d4f3968a1d25d06cc21afccaad
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini-2.local>
2016-01-25 11:49:28 -0500

    get gruntin

--- tasks/infosphere.js
+++ tasks/infosphere.js
@@ -12,8 +12,8 @@ module.exports = function(grunt){
 		}
 
 		for(var i = 8; i<=256; i=i+8){
-			types.push('uint'+i)
-			types.push('int'+i)
+//			types.push('uint'+i)
+//			types.push('int'+i)
 		}
 
 		types.forEach(function(type){
