937578d116efb826b1bdd35d11e4d9ed046d5989
Aakil Fernandes <aakilfernandes@Aakils-Mac-mini-2.local>
2016-02-18 13:25:07 -0500

    grunt.readYAML -> grunt.file.readYAML

--- grunt/config/solc-output-deploy.js
+++ grunt/config/solc-output-deploy.js
@@ -3,7 +3,7 @@ module.exports = function(grunt){
     return {
         "development":{
             "options":{
-                "rpcport":grunt.readYAML('config/geth.YML').development.rpcport
+                "rpcport":grunt.file.readYAML('config/geth.YML').development.rpcport
                 ,"contracts":"generated/contracts.json"
                 ,"chain":"config/development/chain.json"
                 ,"deploy":[
@@ -15,7 +15,7 @@ module.exports = function(grunt){
             }
         },"production":{
             "options":{
-                "rpcport":grunt.readYAML('config/geth.YML').development.production
+                "rpcport":grunt.file.readYAML('config/geth.YML').development.production
                 ,"contracts":"generated/contracts.json"
                 ,"chain":"config/production/chain.json"
                 ,"deploy":[
